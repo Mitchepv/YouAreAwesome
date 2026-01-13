80129cacd28726d213ddbececc3dbe8e926350ad
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-01-13 18:35:17 -0500

    Initial Commit

---
+++ YouareAwesome/YouareAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouareAwesomeApp.swift
+//  YouareAwesome
+//
+//  Created by Nia Mitchell on 1/13/26.
+//
+
+import SwiftUI
+
+@main
+struct YouareAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
