c531c29099e08b91b6fe831280d16d71a20ad8b3
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-01-15 19:35:02 -0500

    switch case and Ternary  operator

--- YouareAwesome/ContentView.swift
+++ YouareAwesome/ContentView.swift
@@ -8,18 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var imageString = ""
+  
     var body: some View {
-            VStack {
-                Image(systemName: "swift")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundStyle(.orange)
-                Text("I am a developer!")
-                    .font(Font.largeTitle)
-                    .fontWeight(.heavy)
-                    .foregroundStyle(.red)
+        VStack {
+            Spacer()
+            
+            Image(systemName:imageString)
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+            
+            Text(message)
+                .font(Font.largeTitle)
+                .fontWeight(.ultraLight)
+
+            
+            Spacer()
+            
+            
+            Button("Press me!") {
+                let message1 = "You are Awesome!"
+                let message2 = " You are Great!"
+                let imageString1 = "hand.thumbsup"
+                let imageString2 = "sun.max.fill"
+                
+                message = ( message == message1 ? message2 : message1 )
+                imageString = ( imageString == imageString1 ? imageString2 : imageString1 )
+                
+//                if message == message1 {
+//                    message = message2
+//                    imageString = imageString2
+//                } else{
+//                    message = message1
+//                    imageString = imageString1
+//                }
+//
             }
-            .padding()
+            .buttonStyle(.borderedProminent)
+            .font(Font.title2)
+            .tint(.orange)
+        }
+        .padding()
+        
     }
 }
 
