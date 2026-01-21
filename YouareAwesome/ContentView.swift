eb6c89bae12b23baee0cb4ce36d98f67b10d2a7d
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-01-21 17:34:39 -0500

    lesson 1.12  repeat while loops

--- YouareAwesome/ContentView.swift
+++ YouareAwesome/ContentView.swift
@@ -10,9 +10,12 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
-    @State private var imageNumber = 0
-    @State private var messageNumber = 0
-  
+    //    @State private var imageNumber = 0
+    //    @State private var messageNumber = 0
+    
+    @State private var lastMessageNumber = -1
+    @State private var lastImageNumber = -1
+    
     var body: some View {
         VStack {
             Spacer()
@@ -31,31 +34,52 @@ struct ContentView: View {
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-                .animation(.bouncy(duration: 0.5, extraBounce: 0.2 ), value: imageName)
+                .animation(.default, value: imageName)
             
-       
-
+            
+            
             
             Spacer()
             
             Button("Show Message!") {
                 
                 let messages = [ "You are Awesome!", "You are Great!", "Wow, you are awesome. Continue to do great things. I'm am so proud of you", "Fabulous, girls!", "Let's do great things", " Be wonderful and lovely", " The smartest in the room" ]
-            
                 
-                message = messages [Int.random(in: 0...messages.count-1)]
-             
-                imageName = "image\(Int.random(in: 0...9))"
-              
+                // randomization
+                // if  messageNumber == lastmessage number keep generating number
+                // set messageString  to message[messageNumber]
+                // lastmessageNumber update with message number
                 
-
+                var messageNumber : Int
+                repeat{
+                    messageNumber = Int.random(in: 0...messages.count-1)
+                } while messageNumber == lastMessageNumber
+                
+                message = messages[messageNumber]
+                lastMessageNumber = messageNumber
+                
+                
+                
+                 /// random image
+                
+                
+                var imageNumber : Int
+               repeat {
+                    imageNumber = Int.random(in: 0...9)
+                } while imageNumber == lastImageNumber
+                imageName = "image\(imageNumber)"
+                lastImageNumber = imageNumber
+                
+                
             }
+            
             .buttonStyle(.borderedProminent)
             .font(Font.title2)
             .tint(.orange)
-        }
-        .padding()
         
+            .padding()
+            
+        }
     }
 }
 
